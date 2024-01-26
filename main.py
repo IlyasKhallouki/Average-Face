ae1ec1f3cd307b73f073cef5dbe99e59f03b460d
Ilyas Khallouki <khalloukielias@gmail.com>
2024-01-26 20:06:17 +0100

    Fixed a minor issue in cropping

--- main.py
+++ main.py
@@ -20,11 +20,13 @@ def crop_and_resize(image, landmarks, image_name, target_size=(600, 600), left_e
 
     eyes_center = np.mean(landmarks[36:48], axis=0)
 
-    angle = np.degrees(np.arctan2(right_eye[1] - left_eye[1], right_eye[0] - left_eye[0]) -
-                    np.arctan2(eyes_center[1] - left_eye[1], eyes_center[0] - left_eye[0]))
+    left_eye = np.array(left_eye)
+    right_eye = np.array(right_eye)
 
-    scale_factor = np.linalg.norm(np.array(right_eye) - np.array(left_eye)) / np.linalg.norm(landmarks[36] - landmarks[45])
-
+    angle = np.degrees(np.arctan2(right_eye[1] - left_eye[1], right_eye[0] - left_eye[0]))
+    eye_distance = np.linalg.norm(right_eye - left_eye)
+    landmarks_distance = np.linalg.norm(landmarks[45] - landmarks[36])
+    scale_factor = eye_distance / landmarks_distance if landmarks_distance != 0 else 1.0
     rotation_matrix = cv2.getRotationMatrix2D(tuple(eyes_center), angle, scale_factor)
 
     cropped_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
@@ -33,15 +35,22 @@ def crop_and_resize(image, landmarks, image_name, target_size=(600, 600), left_e
 
     cropped_image = cv2.warpAffine(cropped_image, np.float32([[1, 0, translation[0]], [0, 1, translation[1]]]), (image.shape[1], image.shape[0]))
 
-    x1 = int(left_eye[0] - target_size[0] / 2)
-    x2 = int(left_eye[0] + target_size[0] / 2)
-    y1 = int(left_eye[1] - target_size[1] / 2)
-    y2 = int(left_eye[1] + target_size[1] / 2)
+    x1 = max(int(left_eye[0] - target_size[0] / 2), 0)
+    x2 = min(int(left_eye[0] + target_size[0] / 2), image.shape[1])
+    y1 = max(int(left_eye[1] - target_size[1] / 2), 0)
+    y2 = min(int(left_eye[1] + target_size[1] / 2), image.shape[0])
 
+    if x1 >= x2 or y1 >= y2:
+        print(f"Invalid cropping coordinates for image {image_name}")
+        return
+    
     cropped_image = cropped_image[y1:y2, x1:x2]
+    print("Cropped image dimensions:", cropped_image.shape)
 
     local_output_path = os.path.join(output_path, image_name)
     cv2.imwrite(local_output_path, cropped_image)
+
+    
 
 for image_file in image_files:
     image_path = os.path.join(folder_path, image_file)
@@ -66,4 +75,4 @@ for image_file in image_files:
             landmarks_for_face.append((x, y))
 
         all_landmarks.append(landmarks_for_face)
-    crop_and_resize(cv2.imread(image_path), landmarks_for_face, image_file)
+        crop_and_resize(frame, landmarks_for_face, image_file)
