3acc5a1b06d172e024285ef4623b8ea38c3e92ea
Ilyas Khallouki <khalloukielias@gmail.com>
2024-01-26 19:56:07 +0100

    main.py can now automatically crop images (unfinished)

--- main.py
+++ main.py
@@ -15,8 +15,35 @@ all_landmarks = []
 # Get a list of all image files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
+def crop_and_resize(image, landmarks, image_name, target_size=(600, 600), left_eye=(180, 200), right_eye=(420, 200)):
+    landmarks = np.array(landmarks)
+
+    eyes_center = np.mean(landmarks[36:48], axis=0)
+
+    angle = np.degrees(np.arctan2(right_eye[1] - left_eye[1], right_eye[0] - left_eye[0]) -
+                    np.arctan2(eyes_center[1] - left_eye[1], eyes_center[0] - left_eye[0]))
+
+    scale_factor = np.linalg.norm(np.array(right_eye) - np.array(left_eye)) / np.linalg.norm(landmarks[36] - landmarks[45])
+
+    rotation_matrix = cv2.getRotationMatrix2D(tuple(eyes_center), angle, scale_factor)
+
+    cropped_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
+
+    translation = (left_eye[0] - eyes_center[0], left_eye[1] - eyes_center[1])
+
+    cropped_image = cv2.warpAffine(cropped_image, np.float32([[1, 0, translation[0]], [0, 1, translation[1]]]), (image.shape[1], image.shape[0]))
+
+    x1 = int(left_eye[0] - target_size[0] / 2)
+    x2 = int(left_eye[0] + target_size[0] / 2)
+    y1 = int(left_eye[1] - target_size[1] / 2)
+    y2 = int(left_eye[1] + target_size[1] / 2)
+
+    cropped_image = cropped_image[y1:y2, x1:x2]
+
+    local_output_path = os.path.join(output_path, image_name)
+    cv2.imwrite(local_output_path, cropped_image)
+
 for image_file in image_files:
-    # Read the image
     image_path = os.path.join(folder_path, image_file)
     frame = cv2.imread(image_path)
 
@@ -26,13 +53,11 @@ for image_file in image_files:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the image
     faces = hog_face_detector(gray)
 
     for face in faces:
         face_landmarks = dlib_facelandmark(gray, face)
 
-        # List to store landmarks for each face
         landmarks_for_face = []
 
         for n in range(68):
@@ -41,18 +66,4 @@ for image_file in image_files:
             landmarks_for_face.append((x, y))
 
         all_landmarks.append(landmarks_for_face)
-
-average_landmarks = []
-
-#Find average landmarks
-for i in range(68):
-    x, y = 0, 0
-    for j in all_landmarks:
-        x+=j[i][0]
-        y+=j[i][1]
-    x/=len(all_landmarks)
-    y/=len(all_landmarks)
-
-    average_landmarks.append((x, y))
-
-print(average_landmarks)
+    crop_and_resize(cv2.imread(image_path), landmarks_for_face, image_file)
